848cb0118135d599dac1d5bb3d57fed97c54fe17
Brendan Magee <bmagee@gmail.com>
2019-04-13 14:55:32 -0400

    Simplify component, implement timer

--- src/App.js
+++ src/App.js
@@ -2,31 +2,61 @@ import React, { Component } from 'react'
 import './main.css'
 class StopWatch extends Component {
   static defaultProps = {
-    buttons: {
-      start: { action: 'toggleTimer', label: 'start' },
-      stop: { action: 'toggleTimer', label: 'stop' },
-      reset: { action: 'resetTimer', label: 'reset' }
-    }
+    interval: 1000
   }
 
   state = {
+    toggleButtonText: 'Start',
     counter: 0
   }
 
+  incrementCounter = () => {
+    this.setState((prev) => ({ counter: prev.counter + 1 }))
+  }
+
+  resetTimer = () => {
+    if (this.state.counter === 0) return null
+
+    this.setState((prev) => {
+      if (prev.timer) {
+        window.clearInterval(prev.timer)
+      }
+      return { timer: null, counter: 0, toggleButtonText: 'Start' }
+    })
+  }
+
+  toggleTimer = () => {
+    this.setState((prev) => {
+      const { timer } = prev
+      if (!timer) {
+        return {
+          timer: window.setInterval(this.incrementCounter, this.props.interval),
+          toggleButtonText: 'Stop'
+        }
+      } else {
+        window.clearInterval(prev.timer)
+        return {
+          timer: null,
+          toggleButtonText: 'Start'
+        }
+      }
+    })
+  }
+
   render() {
-    const { buttons } = this.props
-    const { counter } = this.state
+    const { toggleButtonText, counter } = this.state
     return (
       <div>
-        <div>
+        <div className="counter">
           Counter: {counter}
         </div>
         <div className="buttons">
-          {Object.keys(buttons).map(btnKey => (
-            <button key={btnKey}>
-              {btnKey}
-            </button>
-          ))}
+          <button onClick={() => this.toggleTimer()}>
+            {toggleButtonText}
+          </button>
+          <button onClick={() => this.resetTimer()}>
+            Reset
+          </button>
         </div>
       </div>
     )
