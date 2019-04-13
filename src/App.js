b8a70cd7408a43af9a8e93793e98390d8ff0dd6e
Brendan Magee <bmagee@gmail.com>
2019-04-13 14:18:48 -0400

    Add non-functioning buttons and counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { Component } from 'react'
+import './main.css'
+class StopWatch extends Component {
+  static defaultProps = {
+    buttons: {
+      start: { action: 'toggleTimer', label: 'start' },
+      stop: { action: 'toggleTimer', label: 'stop' },
+      reset: { action: 'resetTimer', label: 'reset' }
+    }
+  }
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+  state = {
+    counter: 0
+  }
+
+  render() {
+    const { buttons } = this.props
+    const { counter } = this.state
+    return (
+      <div>
+        <div>
+          Counter: {counter}
+        </div>
+        <div className="buttons">
+          {Object.keys(buttons).map(btnKey => (
+            <button key={btnKey}>
+              {btnKey}
+            </button>
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+const App = () => (
+  <StopWatch />
+)
+
+export default App
